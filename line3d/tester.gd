298f35c4994eb32193ae966502ce14d5c2adcf5b
rrenode <robert.renodej@gmail.com>
2026-04-03 12:52:43 -0400

    export test selection

--- line3d/tester.gd
+++ line3d/tester.gd
@@ -1,13 +1,21 @@
 ## Testing calling a nim static function of a non-resource
 extends Node3D
 
+enum TESTS { ERR_NO_ADD, WITH_ADD, ERR_NO_ADD_3, ERR_NO_ADD_WITH_NULL, NO_ADD_WITH_FREE}
+@export var testToRun: TESTS = TESTS.ERR_NO_ADD
+
 func _ready() -> void:
-	a()
-	#b()
-	c()
-	d()
-	#e()
-	pass
+	match testToRun:
+		TESTS.ERR_NO_ADD:
+			a()
+		TESTS.WITH_ADD:
+			b()
+		TESTS.ERR_NO_ADD_3:
+			c()
+		TESTS.ERR_NO_ADD_WITH_NULL:
+			d()
+		TESTS.NO_ADD_WITH_FREE:
+			e()
 
 func a() -> void:
 	# 1 RID allocations of type 'N17RendererSceneCull8InstanceE' were leaked at exit.
@@ -36,7 +44,7 @@ func c() -> void:
 	noAdd()
 
 func d() -> void:
-	# 3 RID allocations of type 'N17RendererSceneCull8InstanceE' were leaked at exit.
+	# 1 RID allocations of type 'N17RendererSceneCull8InstanceE' were leaked at exit.
 	# ERROR: Cannot get path of node as it is not in a scene tree.
 	# 	at: (scene/main/node.cpp:2419)
 	# Leaked instance: MeshInstance3D:... - Node path:
