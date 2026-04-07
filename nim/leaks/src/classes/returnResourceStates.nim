298ca4eb45c6f1d6ada6c5e58f54d7c69d029a8e
rrenode <robert.renodej@gmail.com>
2026-04-07 14:00:19 -0400

    removed setget

--- nim/leaks/src/classes/returnResourceStates.nim
+++ nim/leaks/src/classes/returnResourceStates.nim
@@ -2,19 +2,6 @@ import gdext
 import gdext/classes/[gdNode, gdTexture2D, gdSprite2D]
 
 type ReturnResStates* {.gdsync.} = ptr object of Node
-  sprite*: Sprite2D
-
-proc getSprite(self: ReturnResStates): Sprite2D {.gdsync.} =
-  self.sprite
-
-proc setSprite(self: ReturnResStates; value: Sprite2D) {.gdsync.} =
-  self.sprite.queueFree()
-  self.sprite = value
-
-gdexport "sprite",
-  getter = getSprite,
-  setter = setSprite
-
 
 proc createSprite(self: ReturnResStates): Sprite2D {.gdsync.} =
   var s: Sprite2D = instantiate Sprite2D
