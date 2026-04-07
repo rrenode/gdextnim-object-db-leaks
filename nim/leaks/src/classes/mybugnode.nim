f629cd5845ae7bfcbb178e44ea1f944bd262051d
rrenode <robert.renodej@gmail.com>
2026-04-07 14:00:12 -0400

    added unref testing

--- nim/leaks/src/classes/mybugnode.nim
+++ nim/leaks/src/classes/mybugnode.nim
@@ -1,12 +1,14 @@
 import gdext
 import gdext/classes/[
-  gdNode, gdTexture2D, gdResourceLoader
+  gdNode, gdTexture2D, gdResourceLoader, gdRenderingServer, gdInput, gdInputEvent
   ]
 
 type MyBuggedNode* {.gdsync.} = ptr object of Node
   clsProp*: gdref Texture2D
 
 proc setclsProp(self: MyBuggedNode; v: gdref Texture2D) {.gdsync.} =
+  if not self.clsProp[].isNil:
+    discard self.clsProp[].unreference()
   self.clsProp = v
   printRich "WOB: Setting icon texture"
 
@@ -18,5 +20,21 @@ gdexport "clsProp",
   setter = setclsProp
 
 method ready(self: MyBuggedNode) {.gdsync.} =
-  var resource: gdref Resource = ResourceLoader.load("res://icon.svg")
-  self.clsProp = resource as gdref Texture2D
+  #var resource: gdref Resource = ResourceLoader.load("res://icon.svg")
+  #self.clsProp = resource as gdref Texture2D
+  discard
+
+method process(self: MyBuggedNode, delta: float64) {.gdsync.} =
+  discard self.clsProp[].getClass()
+
+method input(self: MyBuggedNode; event: gdref InputEvent) {.gdsync.} = 
+  if event[].isActionPressed("dev_a"):
+    printRich $self.clsProp[].getReferenceCount()
+
+method onDestroy(self: MyBuggedNode) =
+  if not self.clsProp[].isNil:
+    printRich "WOB::::clsProp not nil"
+    echo $self.clsProp[].getReferenceCount()
+    
+    discard self.clsProp[].unreference()
+    
